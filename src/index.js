58e81f5c6cffad4b452c58d0758038e983df6c3c
zordexxx <zordexxx69@gmail.com>
2020-06-25 16:38:57 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('privet')
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+
+console.log(store.getState());
+
+
